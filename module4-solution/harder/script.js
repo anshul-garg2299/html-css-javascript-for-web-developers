40ab83fcbf5bec2691d262464dbaf88efdc74ca9
Anshul Garg <anshulgarg2299@gmail.com>
2020-05-08 12:16:52 +0530

    Added assignment 4 descrpition

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -5,10 +5,10 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 for (var i = 0; i < names.length; i++) {
 
-  /
+  
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+ 
   
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
